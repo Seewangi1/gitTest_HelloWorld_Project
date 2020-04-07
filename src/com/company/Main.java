559cce3dea7500040a4db6ce1ede2b7a6d52d9d4
Seewangi1 <seewangic@gmail.com>
2020-04-07 14:02:44 -0500

    first commit comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main {
 	// write your code here
         System.out.println("hello world ! from github.com..");
         System.out.print("Sandesh Lamsal");
-	
+
+        //
+        int[]  intArray={10,30,40};
+        for (int a:intArray){
+            System.out.println(a);
+        }
+
+       // https://github.com/Seewangi1/gitTest_HelloWorld_Project.git
     }
 }
